5e2f2b3d95cfa91d9cac974a31fba1fda6ff8de4
mjoghenemaega <mjoghenemaega@gmail.com>
2024-07-01 20:25:38 +0100

    added final chnages

--- HngApiMj/settings.py
+++ HngApiMj/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY =config('SECRETKEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','hngtaskone-t20w.onrender.com','.vercel.app']
 
